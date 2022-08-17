afd703a99cbd28baac03d41a1428633cc72e0716
Florian Lueffe <flo@florianlueffe.mwaysolutions.com>
2022-08-17 12:14:12 +0200

    latest contracts evmos

--- components/scFunctions/read/getStatus.tsx
+++ components/scFunctions/read/getStatus.tsx
@@ -3,7 +3,7 @@ import Bountyscape from '../../../utils/Bountyscape.json'
 
 export function GetStatus(ipfsId: string | string[] | undefined) {
   const { chain } = useNetwork()
-  const contractAddr = chain?.name === 'Goerli' ? '0xDFDc2E99A1De4ea9DAf44591fd4d8a1C555F8472' : '0x0140CeC539ee2cf6bECA0597E1eFD2b723A0EBF7'
+  const contractAddr = chain?.name === 'Goerli' ? '0xDFDc2E99A1De4ea9DAf44591fd4d8a1C555F8472' : '0x5A7973aF52BE3A8590b6252F069A1e8502B0a975'
 
   const { data, isLoading, isSuccess } = useContractRead({
     addressOrName: contractAddr,
--- pages/bounties/index.tsx
+++ pages/bounties/index.tsx
@@ -27,7 +27,7 @@ function BountyOverview() {
  const [data, setData] = useState<JSON[]>([]);
 
  const { chain } = useNetwork()
- const contractAddr = chain?.name === 'Goerli' ? '0xDFDc2E99A1De4ea9DAf44591fd4d8a1C555F8472' : '0x0140CeC539ee2cf6bECA0597E1eFD2b723A0EBF7'
+ const contractAddr = chain?.name === 'Goerli' ? '0xDFDc2E99A1De4ea9DAf44591fd4d8a1C555F8472' : '0x5A7973aF52BE3A8590b6252F069A1e8502B0a975'
 
 
 
